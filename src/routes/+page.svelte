2bac70911d6380a0f25dd23800095204d86db48d
Jacob Nielsen <jnillern@gmail.com>
2022-09-15 18:45:07 +0200

    fix up temp frontpage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="flex flex-col gap-4 h-screen justify-center items-center">
+	<img
+		src="https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_60e7569ae9ce4980accd81761f1df220/default/dark/3.0"
+		alt=""
+	/>
+	<span class="text-4xl">Something is coming soon</span>
+</main>
